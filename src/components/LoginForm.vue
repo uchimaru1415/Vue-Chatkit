9b7cf1c0509fa37bf5f458c1b2c05c277818e5a7
takuma <>
2019-11-29 21:14:54 +0900

    ログインフォーム更新

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script>
-import { mapState, mapGetters } from 'vuex'
+import { mapState, mapGetters, mapActions } from 'vuex'
 
 export default {
   name: 'login-form',
@@ -53,6 +53,17 @@ export default {
     ...mapGetters([
       'hasError'
     ])
+  },
+  methods: {
+    ...mapActions([
+      'login'
+    ]),
+    async onSubmit () {
+      const result = await this.login(this.userId)
+      if (result) {
+        this.$router.push('chat')
+      }
+    }
   }
 }
 </script>
